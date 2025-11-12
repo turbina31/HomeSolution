312d48a382235d6013c0fde7738b25defcdf9c3f
Cristian <47560615+turbina31@users.noreply.github.com>
2025-11-12 20:57:41 -0300

    Update Tarea.java

--- HomeSolution/src/entidades/Tarea.java
+++ HomeSolution/src/entidades/Tarea.java
@@ -85,6 +85,10 @@ public class Tarea {
 	public double obtenerDuracionDias() {
 		return duracionDias;
 	}
-    
+    @Override
+    public String toString() {
+        return "Tarea\ntitulo = " + titulo;    
+    }
 }
+
 
